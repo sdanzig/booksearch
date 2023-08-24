ec2642b05c4b9be639de8f333953dfa84564073b
Scott Danzig <2577300+sdanzig@users.noreply.github.com>
2023-08-24 12:43:35 -0400

    Add ability to close modal

--- book-search-app/src/components/BookModal.tsx
+++ book-search-app/src/components/BookModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 type Props = {
   book: Book;
@@ -6,9 +6,23 @@ type Props = {
 };
 
 const BookModal: React.FC<Props> = ({ book, onClose }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClose();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg w-3/4 md:w-1/2 lg:w-1/3 p-8 shadow-2xl">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 overflow-y-auto">
+      <div ref={ref} className="bg-white rounded-lg max-h-full w-3/4 md:w-1/2 lg:w-1/3 p-8 shadow-2xl relative overflow-y-auto">
         <button onClick={onClose} className="absolute top-4 right-4 text-2xl font-bold text-blue-500">
           &times;
         </button>
